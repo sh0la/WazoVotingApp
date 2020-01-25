444c45a15a00a6e9bd926ec6afbd29ae4f6856d5
sh0la <sholaadeniyi@gmail.com>
2020-01-24 22:58:02 -0600

    Create Leaderboard component that controls the cards

---
+++ src/Leaderboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Cards from './Cards';
+
+class Leaderboard extends Component {
+  render() {
+    return (
+      <main>
+        <Cards />
+      </main>
+    )
+  }
+}
+
+export default Leaderboard;
