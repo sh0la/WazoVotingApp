c4325b29f235d2a2ab38925162d487b5a4d1602c
sh0la <sholaadeniyi@gmail.com>
2020-01-24 23:00:00 -0600

    Add header component for the page

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <h1>Wazo</h1>
+      <h3>Idea Voting App</h3>
+      <p>Get collaboration from friends(and foes) to decide your next fun Activity</p>
+    </header>
+  )
+}
+
+export default Header;
