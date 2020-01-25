f6814331763a52cccad8d16dcef356483557b88d
sh0la <sholaadeniyi@gmail.com>
2020-01-24 22:59:08 -0600

    Create preliminary post component

---
+++ src/Cards.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import IdeaImage from './IdeaImage';
+import CountVotes from './CountVotes';
+import IdeaName from './IdeaName';
+import UserImage from './UserImage';
+import IdeaCategory from './IdeaCategory';
+
+
+class Cards extends Component {
+
+  render() {
+    return (
+      <section className="postCard">
+        <IdeaImage />
+        <div className="rightCard">
+          <CountVotes />
+          <IdeaName />
+          <IdeaCategory />
+        </div>
+        <div>
+          <UserImage />
+        </div>       
+      </section>
+    )
+  }
+}
+
+export default Cards;
+
+
+
