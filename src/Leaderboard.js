d35011cfbad2a02c84d0785976a6dfe44941088a
sh0la <sholaadeniyi@gmail.com>
2020-01-27 05:55:14 -0600

    Add mock data for tests

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -2,15 +2,74 @@ import React, { Component } from 'react';
 import Cards from './Cards';
 
 class Leaderboard extends Component {
-  
+  constructor() {
+    super();
+
+    this.state = {
+      vacationSpots: [
+        {
+          id: 1,
+          location: 'Bora Bora, Tahiti',
+          counter: 0,
+          url: 'https://travel.usnews.com/Paris_France/',
+          //votes: CountVotes(),
+          submitterAvatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+          description: 'the black and red sands make for a memorable visit. Next up, indulge in the archaeological delights of the impressively preserved Ancient Akrotiri or hike to Ancient Thera to see the ruins of three empires.',
+          avatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+        },
+        {
+          id: 2,
+          location: 'Cancun, Mexico',
+          counter: 0,
+          url: 'https://travel.usnews.com/Bora_Bora/',
+          //votes: CountVotes(),
+          submitterAvatarUrl: './images/girl.jpg',
+          productImageUrl: './images/cat.jpg',
+          description: 'the black and red sands make for a memorable visit. Next up, indulge in the archaeological delights of the impressively preserved Ancient Akrotiri or hike to Ancient Thera to see the ruins of three empires.',
+          avatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+        },
+        {
+          id: 3,
+          location: 'Rome, Italy',
+          counter: 0,
+          url: 'https://travel.usnews.com/Argentine_Patagonia/',
+          //votes: CountVotes(),
+          submitterAvatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+          description: 'the black and red sands make for a memorable visit. Next up, indulge in the archaeological delights of the impressively preserved Ancient Akrotiri or hike to Ancient Thera to see the ruins of three empires.',
+          avatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+        },
+        {
+          id: 4,
+          location: 'Argentine Patagonia, Argentina',
+          counter: 33,
+          url: 'https://travel.usnews.com/St_Lucia/',
+          //votes: CountVotes(),
+          submitterAvatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+          description: 'the black and red sands make for a memorable visit. Next up, indulge in the archaeological delights of the impressively preserved Ancient Akrotiri or hike to Ancient Thera to see the ruins of three empires.',
+          avatarUrl: './images/boy.jpg',
+          productImageUrl: './images/cat.jpg',
+        }
+      ]
+    }
+
+    //this.state = {counter: 0}
+  }
+
   handleCounter () {
-    this.setState({counter: this.state.counter + 1})
+    this.setState({counter: this.state.vacationSpots.counter + 1})
   }
 
   render() {
+    console.log(this.state.vacationSpots)
     return (
       <main>
-        <Cards sumVotes={this.props.handleCounter}/>
+        <Cards spots={this.state.vacationSpots}/>
       </main>
     )
   }
